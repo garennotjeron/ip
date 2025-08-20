05b0511f72ab89a19948c18a80599fbba064655f
Gerron <gerronleeyf02@gmail.com>
2025-08-20 16:13:32 +0800

    Made changes to intro message

--- src/main/java/DK.java
+++ src/main/java/DK.java
@@ -1,10 +1,22 @@
-public class Duke {
+public class DK {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLine();
+        printIntro();
+        printLine();
+        printEnding();
+        printLine();
+    }
+
+    public static void printIntro() {
+        System.out.println("Hello! I'm DK \nWhat can I do for you?");
+    }
+
+    public static void printEnding() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLine() {
+        System.out.println("__________________________________");
     }
 }
+
