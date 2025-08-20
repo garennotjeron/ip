4ecd0639caf32a27c5cb6eae1ac69828ec45fb06
Gerron <gerronleeyf02@gmail.com>
2025-08-20 18:02:50 +0800

    Updated fields and methods in the 3 new classes, as well as toString for all classes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,6 +18,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return ". [" + this.getCompletion() + "] " + this.description;
+        return "[" + this.getCompletion() + "] " + this.description;
     }
 }
