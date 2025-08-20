e44103c00bcad637c2a7e252deb5e9280f8313d6
Gerron <gerronleeyf02@gmail.com>
2025-08-20 16:25:09 +0800

    Reformatted Line Printing in functions

--- src/main/java/DK.java
+++ src/main/java/DK.java
@@ -2,17 +2,17 @@ public class DK {
     public static void main(String[] args) {
         printLine();
         printIntro();
-        printLine();
         printEnding();
-        printLine();
     }
 
     public static void printIntro() {
         System.out.println("Hello! I'm DK \nWhat can I do for you?");
+        printLine();
     }
 
     public static void printEnding() {
         System.out.println("Bye. Hope to see you again soon!");
+        printLine();
     }
 
     public static void printLine() {
