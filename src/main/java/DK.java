71dfd05526d3a6dd9b92c111edaf0228cb74a26c
Gerron <gerronleeyf02@gmail.com>
2025-08-20 18:30:20 +0800

    added support for tracking three different types of tasks

--- src/main/java/DK.java
+++ src/main/java/DK.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class DK {
 
-    private static final List<Task> allInputs = new ArrayList<>(); // limit set for size of array for all user inputs
+    private static final List<Task> allTasks = new ArrayList<>(); // limit set for size of array for all user inputs
 
     public static void main(String[] args) {
         printLine();
@@ -42,35 +42,64 @@ public class DK {
             } else if (userInput.startsWith("unmark ")) {
                 int index = Integer.parseInt(userInput.substring(7).trim());
                 unmarkItem(index);
+            } else if (userInput.startsWith("todo ") || userInput.startsWith("deadline ") || userInput.startsWith("event ")) {
+                addItem(userInput);
             }
             else {
-                allInputs.add(new Task(userInput));
-                System.out.println("added: " + userInput);
+                System.out.println("Invalid input, please try again");
             }
         }
     }
 
     public static void displayList() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i < allInputs.size() + 1; i++) {
-            System.out.println(i + allInputs.get(i-1).toString());
+        for (int i = 1; i < allTasks.size() + 1; i++) {
+            System.out.println(i + "." + allTasks.get(i-1).toString());
         }
         printLine();
     }
 
     public static void markItem(int index) {
-        Task t = allInputs.get(index - 1);
-        t.updateCompletion();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(index + t.toString());
+        Task t = allTasks.get(index - 1);
+        if (t.getCompletion().equals(" ")) {
+            t.updateCompletion();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(index + "." + t.toString());
+        } else {
+            System.out.println("This task has already been marked as done");
+        }
         printLine();
     }
 
     public static void unmarkItem(int index) {
-        Task t = allInputs.get(index - 1);
-        t.updateCompletion();
-        System.out.println("Nice! I've marked this task as not done yet:");
-        System.out.println(index + t.toString());
+        Task t = allTasks.get(index - 1);
+        if (t.getCompletion().equals("X")) {
+            t.updateCompletion();
+            System.out.println("Nice! I've marked this task as not done yet:");
+            System.out.println(index + "." + t.toString());
+        } else {
+            System.out.println("This task has already been marked as not done yet");
+        }
+        printLine();
+    }
+
+    public static void addItem(String userInput) {
+        Task newTask;
+        if (userInput.startsWith("todo ")) {
+            newTask = new Todo(userInput.substring(5));
+        } else if (userInput.startsWith("deadline ")) {
+            String[] splitted = userInput.split("/");
+            newTask = new Deadline(splitted[0].substring(9).trim(), splitted[1].substring(3).trim());
+        } else {
+            String[] splitted = userInput.split("/");
+            newTask = new Event(splitted[0].substring(6).trim(),
+                                splitted[1].substring(5).trim(),
+                                splitted[2].substring(3).trim());
+        }
+        allTasks.add(newTask);
+        System.out.println("Got it. I've added this task:\n" + newTask.toString());
+        String s = allTasks.size() == 1 ? "" : "s";
+        System.out.println("Now you have " + allTasks.size() + " task" + s + " in the list.");
         printLine();
     }
 
