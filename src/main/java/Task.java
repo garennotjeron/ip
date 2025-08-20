85245931e822a84afe8abf98bd0b224afb4cc736
Gerron <gerronleeyf02@gmail.com>
2025-08-20 17:14:37 +0800

    Updated fields and methods in the Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void updateCompletion() {
+        this.isCompleted = !isCompleted;
+    }
+
+    public String getCompletion() {
+        return isCompleted ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return ". [" + this.getCompletion() + "] " + this.description;
+    }
 }
