78c0d06ae2994ddce06397f6e28815c232b9841b
Gerron <gerronleeyf02@gmail.com>
2025-08-25 00:49:06 +0800

    Created Exception class for DK Chatbot

---
+++ src/main/java/DKException.java
@@ -0,0 +1,11 @@
+public class DKException extends Exception{
+
+    public DKException (String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "DK has detected this error:" + this.getMessage();
+    }
+}
