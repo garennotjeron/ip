2c62dea68b43e6042474769a21e067ce5fd1abb5
Gerron <gerronleeyf02@gmail.com>
2025-08-30 01:15:49 +0800

    Update import statements and unused variables

--- src/main/java/DK.java
+++ src/main/java/DK.java
@@ -1,15 +1,9 @@
-import java.io.IOException;
-import java.time.LocalDate;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.List;
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class DK {
 
-    private static final List<Task> allTasks = new ArrayList<>(); // limit set for size of array for all user inputs
     private static final String FILEPATH = "data";
     private static final String FILENAME = FILEPATH + "/allTasks.txt";
 
