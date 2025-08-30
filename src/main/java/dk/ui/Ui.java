c0cdc7bcdb42f09cac9e8b31e4523f2d7c8a2efd
Gerron <gerronleeyf02@gmail.com>
2025-08-31 00:27:57 +0800

    Add method to change intro printing

--- src/main/java/dk/ui/Ui.java
+++ src/main/java/dk/ui/Ui.java
@@ -41,7 +41,7 @@ public class Ui {
 
     public void printIntro() {
         printLine();
-        System.out.println("Hello! I'm dk.DK\nWhat can I do for you?");
+        System.out.println("Hello! I'm DK\nWhat can I do for you?");
         printLine();
     }
 
