19031fddd5b7f3f70077a65da5e21fdc2f649bff
Gerron <gerronleeyf02@gmail.com>
2025-08-31 19:20:40 +0800

    Enable users to find tasks via searching for keyword in description

--- src/main/java/dk/tasks/TaskList.java
+++ src/main/java/dk/tasks/TaskList.java
@@ -1,5 +1,6 @@
 package dk.tasks;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
@@ -32,13 +33,30 @@ public class TaskList {
         return this.allTasks.isEmpty();
     }
 
+    /**
+     * Filters and displays all tasks in the current list that include the keyword given by the user.
+     * @param keyword The keyword to be checked in each task's description
+     * @return A list of tasks that include the keyword in its description
+     */
+    public List<Task> filterTasks(String keyword) {
+        List<Task> filtered = new ArrayList<>();
+        for (Task t: this.allTasks) {
+            if (t.getDescription().contains(keyword)) {
+                filtered.add(t);
+            }
+        }
+        return filtered;
+    }
+
     @Override
     public String toString() {
-        String result = "";
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 1; i < allTasks.size() + 1; i++ ) {
-            stringBuilder.append(String.format(i + "." + allTasks.get(i-1).toString() + "\n"));
+            stringBuilder.append(String.format(i + "." + allTasks.get(i-1).toString()));
+            if (i < allTasks.size()) {
+                stringBuilder.append("\n");
+            }
         }
-        return result;
+        return stringBuilder.toString();
     }
 }
