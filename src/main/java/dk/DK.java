ce524d8bac2a470dd174c95f3de02dfc26c1089c
Gerron <gerronleeyf02@gmail.com>
2025-08-31 17:03:28 +0800

    Update constants naming

--- src/main/java/dk/DK.java
+++ src/main/java/dk/DK.java
@@ -5,12 +5,11 @@ import dk.ui.Ui;
 
 public class DK {
 
-    private static final String FILEPATH = "data";
-    private static final String FILENAME = FILEPATH + "/allTasks.txt";
+    private static final String FILE_PATH = "data";
+    private static final String FILE_NAME = FILE_PATH + "/allTasks.txt";
 
     public static void main(String[] args) {
-
-        Storage storage = new Storage(FILEPATH,FILENAME);
+        Storage storage = new Storage(FILE_PATH, FILE_NAME);
         Ui ui = new Ui(storage);
         ui.start();
     }
