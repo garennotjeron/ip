6cbaf70636da29bbd0c8189f8bac02d955efe236
Gerron <gerronleeyf02@gmail.com>
2025-09-04 21:30:04 +0800

    Add GUI capabilities and allow launching of GUI

--- src/main/java/dk/DK.java
+++ src/main/java/dk/DK.java
@@ -1,17 +1,41 @@
 package dk;
 
+import dk.parsers.Parser;
 import dk.storage.Storage;
+import dk.ui.Main;
 import dk.ui.Ui;
+import javafx.application.Application;
 
 public class DK {
 
     private static final String FILE_PATH = "data";
     private static final String FILE_NAME = FILE_PATH + "/allTasks.txt";
+    private static Parser parser;
 
     public static void main(String[] args) {
+        setUp();
+        startGui();
+    }
+
+    public static void setUp() {
         Storage storage = new Storage(FILE_PATH, FILE_NAME);
-        Ui ui = new Ui(storage);
+        Ui ui =  new Ui (storage);
+        parser = new Parser(storage);
+    }
+
+    public static void startTextUi(Ui ui){
         ui.start();
+    }
+
+    public static void startGui() {
+        Application.launch(Main.class);
+    }
+
+    public String getResponse(String input) {
+        if (input.equals("bye")) {
+            System.exit(0);
+        }
+        return parser.executeCommand(input);
     }
 }
 
