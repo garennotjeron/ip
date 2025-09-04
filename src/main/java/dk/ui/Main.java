7602761d26edf3ac0c2512eb8edad53ca332fb2d
Gerron <gerronleeyf02@gmail.com>
2025-09-04 21:28:26 +0800

    Add main.java for implementation of GUI

---
+++ src/main/java/dk/ui/Main.java
@@ -0,0 +1,32 @@
+package dk.ui;
+
+import dk.DK;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private DK dk = new DK();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            fxmlLoader.<MainWindow>getController().setDk(dk);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
