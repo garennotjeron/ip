7b271b09f177c7c98a246912f5b3919ddbaa867a
Gerron <gerronleeyf02@gmail.com>
2025-09-04 21:41:30 +0800

    Update error message printing for DialogBox.java

--- src/main/java/dk/ui/DialogBox.java
+++ src/main/java/dk/ui/DialogBox.java
@@ -27,7 +27,7 @@ public class DialogBox extends HBox{
             fxmlLoader.setRoot(this);
             fxmlLoader.load();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
         dialog.setText(text);
